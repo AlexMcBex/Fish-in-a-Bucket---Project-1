3c04efdd74e29073f4d030dc59e0eaba5f2a2638
AlexMcBex <alessiobelli94@gmail.com>
2023-01-03 11:48:08 -0500

    started JS, made the bucket movable

--- js/app.js
+++ js/app.js
@@ -24,3 +24,105 @@
         // if a collision is detected gameover is activated
 // once the game ends we have a display of the score and a new game button
     // an alert should suffice but a popup would be better
+////////////////
+
+//////HTML Elements ///////
+// canvas
+const game = document.getElementById("river")
+// score
+const score = document.getElementById("score")
+// lost fish
+const lostFish = document.getElementById("lost-score")
+
+const ctx = game.getContext("2d")
+
+game.setAttribute('width', getComputedStyle(game)['width'])
+game.setAttribute('height', getComputedStyle(game)['height'])
+// game.width = 800
+
+
+// const player = new Bucket(265, 800, 70, 90, 'brown')
+//bucket
+const player = {
+    x : 265,
+    y : 800,
+    width : 70,
+    height : 90,
+    color : "brown",
+    alive : true,
+    speed : 15,
+    direction :{
+        left: false,
+        right: false
+    },
+    setDirection : function (key) {
+        console.log('this is the key in setDirection', key)
+        if (key.toLowerCase() == 'a') { this.direction.left = true }
+        if (key.toLowerCase() == 'd') { this.direction.right = true }
+    },
+    unsetDirection: function (key) {
+        console.log('this is the key in unsetDirection', key)
+        if (key.toLowerCase() == 'a') { this.direction.left = false }
+        if (key.toLowerCase() == 'd') { this.direction.right = false }
+    },
+    movePlayer: function () {
+        if (this.direction.left) {
+            this.x -= this.speed
+            if (this.x <= 0) {
+                this.x = 0
+            }
+        }
+        if (this.direction.right) {
+            this.x += this.speed
+            if (this.x + this.width >= game.width) {
+                this.x = game.width - this.width
+            }
+        }
+    },
+    render: function () {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+//         ctx.drawBucket = () =>{
+// ctx.beginPath();
+// ctx.moveTo(0, 0);
+// ctx.moveTo(70, 0);
+// ctx.lineTo(70, 5);
+// ctx.lineTo(60, 60);
+// ctx.lineTo(10, 60);
+// ctx.lineTo(0, 5);
+// ctx.fill();
+    }
+}
+    
+
+
+
+
+
+const gameLoop = () => {
+    ctx.clearRect(0, 0, game.width, game.height)
+
+    player.render()
+    player.movePlayer()
+    // movement.textContent = `${player.x}, ${player.y}`
+}
+
+
+
+// keydown will set a player's direction
+document.addEventListener('keydown', (e) => {
+    player.setDirection(e.key)
+})
+document.addEventListener('keyup', (e) => {
+    if(['a', 'd'].includes(e.key)) {
+        player.unsetDirection(e.key)
+    }
+})
+
+const gameInterval = setInterval(gameLoop, 60)
+
+const stopGameLoop = () => { clearInterval(gameInterval) }
+
+document.addEventListener('DOMContentLoaded', function () {
+    gameInterval
+})
